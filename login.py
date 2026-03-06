a0a8cdc9c100e63d57466ca36ad0e390fefeb43f
Victor97 <victoralejandro971@hotmail.com>
2026-03-06 11:54:26 -0600

    Funcionamiento del login terminado, ahora se esta dando diseño

--- login.py
+++ login.py
@@ -1,34 +1,61 @@
 import tkinter as tk
 from tkinter import messagebox
 from principal import Principal
+import csv
+
 
 
 class Login(tk.Frame):
     def __init__(self, parent, controlador):
-        super().__init__(parent,bg="#050034")
+        super().__init__(parent,bg="#050520")
         self.controlador = controlador
-        self.label_texto1= tk.Label(self, text="Inicio de sesion").pack(pady=10)
 
-        tk.Label(self, text="Usuario").pack()
-        self.entry_usuario = tk.Entry(self)
-        self.entry_usuario.pack(pady=10)
+        
+        bg_color = "#050520"
+        lbl_font = ("Segoe UI", 12)
+        title_font = ("Segoe UI", 24, "bold")
 
-        self.label_texto3= tk.Label(self, text="Contraseña").pack()
-        self.entry_pass = tk.Entry(self)
-        self.entry_pass.pack(pady=10)
+        #Titulo
+        self.label_texto1= tk.Label(self, text="INICIO DE SESIÓN", bg=bg_color, fg="#ffffff").pack(pady=(0, 40))
 
-        self.boton_inicio= tk.Button(self, text="Iniciar sesion", command = self.verificar_usuario).pack()
+        #Usuario
+        tk.Label(self, text="Usuario", font=title_font).pack()
+        self.entry_usuario = tk.Entry(self, font=("Arial", 12), bd=0, highlightthickness=1)
+        self.entry_usuario.pack(ipady=8, pady=(5, 20))
+
+        #Contraseña
+        self.label_texto3= tk.Label(self, text="Contraseña", font=title_font).pack()
+        self.entry_pass = tk.Entry(self, show="*",font=("Arial", 12), bd=0, highlightthickness=1)
+        self.entry_pass.pack(ipady=8, pady=(5, 10))
+
+        self.check_var = tk.BooleanVar()
+
+        check = tk.Checkbutton(self, text="Mostrar contraseña",variable=self.check_var,command=self.mostrar_password, bg=bg_color, fg="white", 
+                           selectcolor="#050520", activebackground=bg_color, 
+                           activeforeground="white")
+        check.pack(anchor="w", pady=(0, 30))
+
+        self.boton_inicio= tk.Button(self, text="Iniciar sesion", command = self.verificar_usuario, font=("Segoe UI", 12, "bold"),
+                      bg="#0078d7", fg="white", bd=0, cursor="hand2",
+                      activebackground="#005a9e", activeforeground="white").pack(ipady=10)
 
 
     def verificar_usuario(self):
 
         usuario = self.entry_usuario.get()
         password = self.entry_pass.get()
+        try:
+            with open("usuarios.csv", mode ="r", newline="", encoding="utf-8") as file:
 
-        if usuario == "" and password == "":
-            self.mostrar_principal()
-        else:
-            messagebox.showinfo("Error", "Inicio de sesion incorrecto.")
+                reader= csv.reader(file)   
+
+                for row in reader:
+                    if row[1]== usuario and row[3]== password:
+                        self.mostrar_principal()
+                        break   
+        except FileNotFoundError:
+            print("Error: No se encontró el archivo 'usuarios.csv'.")
+            return
 
 
     def mostrar_principal(self):
@@ -36,6 +63,12 @@ class Login(tk.Frame):
                 self.pack_forget()
                 self.frame_actual = Principal(self.master, self.controlador)
                 self.frame_actual.pack(fill="both", expand=True)
+
+    def mostrar_password(self):
+        if self.check_var.get():
+            self.entry_pass.config(show="")
+        else:
+            self.entry_pass.config(show="*")
 
             
 
